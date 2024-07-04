5d465dabb86d5bd93994fa55304fdbb61d8fb91d
Adarsh <singhadarsh100@gmail.com>
2024-07-04 23:47:39 +0530

    cors management

--- app.js
+++ app.js
@@ -26,7 +26,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "https://id-ocr-application.vercel.app/",
+  }
+));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: false, limit: "2mb" }));
 
