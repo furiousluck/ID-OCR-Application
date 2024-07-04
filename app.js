0bd136122e90ad7359e1fdac4073e8d0ce512c38
Adarsh <singhadarsh100@gmail.com>
2024-07-04 23:44:06 +0530

    some change

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_URI).then(console.log("DB Connected"));
 
+app.use((req, res, next) => {
+  if (req.headers.host === "project-1-i21e.onrender.com") {
+    return res.redirect(301, "https://id-ocr-application.vercel.app/");
+  }
+  next();
+});
+
 app.use(cors());
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: false, limit: "2mb" }));
