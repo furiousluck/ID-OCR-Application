34d92489b14062527d5980ffe92ffff95b07e62b
Adarsh <singhadarsh100@gmail.com>
2024-07-05 00:10:40 +0530

    changes :(

--- app.js
+++ app.js
@@ -19,11 +19,7 @@ require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_URI).then(console.log("DB Connected"));
 
-app.use(cors(
-  {
-    origin: "*",
-  }
-));
+app.use(cors());
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: false, limit: "2mb" }));
 
